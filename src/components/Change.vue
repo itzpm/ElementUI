31fe86c8730c61fa84054b63d36f6a00a3ba958b
zhupeimeng <577855901@qq.com>
2020-11-20 11:46:18 +0800

    second commit

---
+++ src/components/Change.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in pics" :key="item.url">
+        <img :src="item.url"/>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Change",
+  data() {
+    return {
+      pics: [
+        { url: require("../assets/logo.png") },
+        { url: require("../assets/logo.png") },
+      ]
+    };
+  }
+}
+</script>
+
+<style scoped>
+.el-carousel__item img {
+  height: 500px;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+</style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <i class="el-icon-edit"></i>
+  <i class="el-icon-share"></i>
+  <i class="el-icon-delete"></i>
+  <i class="el-icon-platform-eleme"></i>
+  <i class="el-icon-loading"></i>
+  <el-button type="primary" icon="el-icon-search">搜索</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Icon"
+}
+</script>
+
+<style scoped>
+i{
+  padding: 0 5px;
+}
+</style>
